0f1db16f9d8f188bffbe3f4b01a3430142eb4188
AlexDv <DvoinikovAV@yandex.ru>
2024-01-22 13:22:53 +0300

    Задание 1

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        int [] arr=new int[3];
+        arr[0]=1;
+        arr[1]=2;
+        arr[2]=3;
+
+        double[] arr1={1.57, 7.654, 9.986};
+        char[] symArr={'П','р','в','е','т'};
+
+
+        System.out.println("Hello world!");
+    }
+}
