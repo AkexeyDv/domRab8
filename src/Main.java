48b8f81710f759e65437be8d2090c663639f570b
AlexDv <DvoinikovAV@yandex.ru>
2024-01-22 13:51:57 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,32 @@ public class Main {
         arr[2]=3;
 
         double[] arr1={1.57, 7.654, 9.986};
-        char[] symArr={'П','р','в','е','т'};
+        char[] symArr={'П','р','и','в','е','т'};
 
         //Задание 2
+        for(int i=0;i<=2;i++){
+            System.out.print(arr[i]);
+            if (i!=2){
+                System.out.print(", ");
+            }
 
-        System.out.println("Hello world!");
+        }
+        System.out.println();
+        for(int i=0;i<=2;i++){
+            System.out.print(arr1[i]);
+            if (i!=2){
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        for(int i=0;i<=5;i++){
+            System.out.print(symArr[i]);
+            if (i!=5){
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+
+
     }
 }
